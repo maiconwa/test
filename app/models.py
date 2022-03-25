ab2c82903e9b7f611316f308287b41275292d8eb
Maicon William <maicon.w.a@gmail.com>
2022-03-25 07:57:03 -0300

    added utils.py

--- app/models.py
+++ app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+#'cpf',
+#'private',
+#'incompleto',
+#'data da ultima compra',
+#'ticket medio',
+#'ticket da ultima compra',
+#'loja mais frequente',
+#'loja da ultima compra',
+#'cpf valido'
