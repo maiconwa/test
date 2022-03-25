ee9ec50fa3c68fb4a4fb48f33d28e031ee6230d6
Maicon William <maicon.w.a@gmail.com>
2022-03-25 08:37:16 -0300

    changed models and template

--- app/models.py
+++ app/models.py
@@ -1,12 +1,16 @@
 from django.db import models
 
-# Create your models here.
-#'cpf',
-#'private',
-#'incompleto',
-#'data da ultima compra',
-#'ticket medio',
-#'ticket da ultima compra',
-#'loja mais frequente',
-#'loja da ultima compra',
-#'cpf valido'
+from django.db import models
+
+class customer(models.Model):
+  cpf = models.CharField(max_length=14)
+  private = models.DecimalField(max_digits=5, decimal_places=2)
+  incompleto = models.DecimalField(max_digits=5, decimal_places=2)
+  data_da_ultima_compra = models.DateField()
+  ticket_medio = models.FloatField()
+  ticket_da_ultima_compra = models.FloatField()
+  loja_mais_frequente = models.CharField(max_length=18)
+  loja_da_ultima_compra = models.CharField(max_length=18)
+
+  def __str__(self):
+    return self.cpf
